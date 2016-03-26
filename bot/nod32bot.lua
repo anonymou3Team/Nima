30ce0ba3f8e7d017c990cb71ff4bada8461c5221
anonymou3Team <anonymou3nk@gmail.com>
2016-03-26 16:55:12 +0430

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -245,7 +245,7 @@ function create_config( )
     "time",
     "version"
     },
-	    sudo_users = {119408522},--Sudo users
+	    sudo_users = {119408525},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
